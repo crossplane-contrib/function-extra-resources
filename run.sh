f140044daa8e79f0a6291710c48281c8e9d690c9
Joe Bowbeer <joe.bowbeer@gmail.com>
2025-07-17 05:27:28 +0000

    fix: add namespace to schema
    Signed-off-by: Joe Bowbeer <joe.bowbeer@gmail.com>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # It seems extra-resources must be in only one file.
 # Supplying the argument a second time excludes any input from the 
 # first --extra-resources argument
-crossplane beta render \
+crossplane render \
   --extra-resources example/extraResources.yaml \
   --include-context \
   example/xr.yaml example/composition.yaml example/functions.yaml
